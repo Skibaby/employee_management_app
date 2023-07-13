86817756bf798354386591e719dcf4a3888652bf
yuski <yuskijoness@gmail.com>
2023-07-13 19:47:11 -0400

    added seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE employee_db;
+
+INSERT INTO departments (name)
+    VALUES ('Customer Service'), ('Marketing');
+
+    INSERT INTO roles (title, salary);
+        VALUES ('Receptionist', 60000), ('Tele-Marketer, 70000');
+
+        INSERT INTO employees (first_name, last_name)
+            VALUES ('Henry', 'Weaver'), ('Gary', 'Crocket'), ('Barry', 'Allen');
+
+
